fa4ec870866ce53080054433acccbfd2cf4f896a
Joanna Kaminska <joanna8162@gmail.com>
2025-08-12 17:28:56 +0200

    Notebook - zadanie

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,11 +1,29 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
 
-        if (1 > 2){
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
-        }
-        }
+
+       Notebook notebook = new Notebook(600, 1000, 2023);
+        System.out.println("notebook.weight: " + notebook.weight + ", notebook.price: " + notebook.price + ", year of release: " + notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.bestOption();
+       Notebook heavyNotebook = new Notebook(2000, 1500, 2020);
+        System.out.println("notebook.weight: " + heavyNotebook.weight + ", notebook.price: " + heavyNotebook.price + ", year of release: " + heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.bestOption();
+       Notebook oldNotebook = new Notebook(1200, 500, 2015);
+        System.out.println("notebook.weight: " + oldNotebook.weight + ", notebook.price: " + oldNotebook.price + ", year of release: " + oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.bestOption();
+        Notebook newNotebook = new Notebook(450, 2569, 2025);
+        System.out.println("notebook.weight: " + newNotebook.weight + ", notebook.price: " + newNotebook.price + ", year of release: " + newNotebook.year);
+        newNotebook.checkPrice();
+        newNotebook.checkWeight();
+        newNotebook.bestOption();
+
+
+
     }
+}
