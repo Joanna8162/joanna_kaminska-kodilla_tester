c59e478babbbea3b6ad663c9500ae0be54859023
Joanna Kaminska <joanna8162@gmail.com>
2025-08-22 12:47:26 +0200

    Calculator - corrections

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,44 +1,39 @@
 package com.kodilla.basic_assertion;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
 
     Calculator calculator = new Calculator();
-    int a = 5;
-    int b = 8;
-    double c = 0;
-    double d = -3;
-    double e = 9;
 
     @Test
     public void testSum() {
-        int sumResult = calculator.sum(a, b);
-        assertEquals(13, sumResult);
+        double result = calculator.sum(5, 8);
+        assertEquals(13, result, 0.0001);
     }
 
     @Test
-    public void testSubstract() {
-        int subtractResult = calculator.subtract(a, b);
-        assertEquals(-3, subtractResult);
+    public void testSubtract() {
+        double result = calculator.subtract(5, 8);
+        assertEquals(-3, result, 0.0001);
     }
+
     @Test
-        public void testSquare1() {
-            double squareResult = calculator.squared(c);
-            assertEquals(0, squareResult, 0.001);
-        }
+    public void testSquare1() {
+        double result = calculator.square(0);
+        assertEquals(0, result, 0.0001);
+    }
 
     @Test
     public void testSquare2() {
-        double squareResult = calculator.squared(d);
-        assertEquals(9, squareResult, 0.001);
+        double result = calculator.square(-3);
+        assertEquals(9, result, 0.0001);
     }
 
     @Test
     public void testSquare3() {
-        double squareResult = calculator.squared(e);
-        assertEquals(81, squareResult, 0.001);
+        double result = calculator.square(9);
+        assertEquals(81, result, 0.0001);
     }
 }
