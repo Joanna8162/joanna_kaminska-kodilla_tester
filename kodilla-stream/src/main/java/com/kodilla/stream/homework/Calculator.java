cf343dbe41f52369c6c5285a27909354e8c9bb32
Joanna Kaminska <joanna8162@gmail.com>
2025-09-12 18:32:41 +0200

    exceptions-homework

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.homework;
+
+
+public class Calculator {
+
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public String add(String a, String b) {
+        return a + b;
+    }
+}
